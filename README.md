0eb67f6d1370466445318b3fc4446eda24f7bd2f
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2024-01-31 14:13:30 +0530

    Adding Vue Spreadsheet getting started sample
    Adding Vue Spreadsheet getting started sample

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# getting-started-with-the-vue-spreadsheet-component
+# Getting Started with the Vue Spreadsheet Component
+
 A quick-start project that helps you create and configure the Syncfusion Vue Spreadsheet component. This project contains code snippets for data binding, as well as importing and exporting Excel files from the Spreadsheet.
+
+The getting started documentation for the Syncfusion Vue Spreadsheet component: 
+https://ej2.syncfusion.com/vue/documentation/spreadsheet/vue-3-getting-started
+
+Check out this online example of the quick info template in the Syncfusion Vue Spreadsheet component:
+https://ej2.syncfusion.com/vue/demos/#/material3/spreadsheet/default.html 
+
+Tutorial video: https://www.syncfusion.com/tutorial-videos  
+
+## Project prerequisites
+
+### Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+### Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+
+## How to run this application?
+
+To run this application, you need to clone the `getting-started-with-the-vue-spreadsheet-component` repository and then open it in Visual Studio Code. Now, simply install all the necessary packages into your current project using the `npm install` command and run your project using the `npm run dev` command.
+
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script>
+  import { SpreadsheetComponent, SheetsDirective, SheetDirective,
+      RangesDirective, RangeDirective, ColumnsDirective,
+      ColumnDirective } from '@syncfusion/ej2-vue-spreadsheet';
+  import datasource from './default.json';
+  export default{
+    components: {
+      'ejs-spreadsheet': SpreadsheetComponent,
+      'e-sheets': SheetsDirective,
+      'e-sheet': SheetDirective,
+      'e-ranges': RangesDirective,
+      'e-range': RangeDirective,
+      'e-columns': ColumnsDirective,
+      'e-column': ColumnDirective
+    },
+    data(){
+      return{
+        data: datasource.defaultData,
+        customWidth: 130,
+        openUrl: 'https://services.syncfusion.com/vue/production/api/spreadsheet/open',
+        saveUrl: 'https://services.syncfusion.com/vue/production/api/spreadsheet/save'
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <ejs-spreadsheet :openUrl="openUrl" :saveUrl="saveUrl">
+      <e-sheets>
+        <e-sheet>
+          <e-columns>
+            <e-column :width="customWidth"></e-column>
+            <e-column :width="customWidth"></e-column>
+            <e-column :width="customWidth"></e-column>
+            <e-column :width="customWidth"></e-column>
+            <e-column :width="customWidth"></e-column>
+            <e-column :width="customWidth"></e-column>
+          </e-columns>
+          <e-ranges>
+            <e-range :dataSource="data"></e-range>
+          </e-ranges>
+        </e-sheet>
+      </e-sheets>
+    </ejs-spreadsheet>
+  </div>
+</template>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-grids/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-spreadsheet/styles/material.css";
+</style>
